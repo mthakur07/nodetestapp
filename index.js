6f878ca9237493f13ff5ba221b1c89713b2f65d9
mthakur07 <work.arun37@gmail.com>
2024-02-08 17:36:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')  
+// import 'dotenv/config'  - // you can also write like this
+require('dotenv').config() ; 
+ 
+const app = express()
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/about', (req, res) => {
+    res.send("this is about Routes!!")
+})
+app.get('/login', (req, res) => {
+    //res.send(" Welcome to logging in!")
+    res.send(`<h1>This is h1 tag</h1>`)
+});
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
+
+
+
+
